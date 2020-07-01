15d61a79e681a794f2b3e72a12425962d807296f
khairul-abdi <kabdi384@gmail.com>
2020-07-01 15:42:18 +0700

    vuex-state-managements---project-setup

---
+++ vuex-state-managements/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <AppHeader />
+    <div class="container">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vuex-state-managements/src/components/ProductList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="d-flex align-items-stretch flex-wrap">
+    <ProductCart />
+    <ProductCart />
+    <ProductCart />
+    <ProductCart />
+    <ProductCart />
+  </div>
+</template>
+
+<script>
+import ProductCart from './ProductCart';
+export default {
+  components: {
+    ProductCart
+  }
+}
+</script>
+
+<style>
+
+</style>
